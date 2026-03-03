c070db32c43f711dedf3104f13f9ad76e6f5f032
Danilo Chaves <danchavesds@gmail.com>
2026-03-03 20:49:01 -0300

    first commit

---
+++ src/main/java/com/dnilochaves/wayprice_core/WaypriceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.dnilochaves.wayprice_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaypriceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaypriceCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnilochaves/wayprice_core/WaypriceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnilochaves.wayprice_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaypriceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
